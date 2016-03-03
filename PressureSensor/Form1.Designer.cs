cdb18b12c25d358eb49960ba78f0ffeede951c20
jrenzas <jrenzas@gmail.com>
2016-03-03 10:40:53 -0800

    Initial file dump.

---
+++ PressureSensor/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace PressureSensor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelUnits = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.labelPressure = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDownAlarm = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAlarm)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe Script", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(258, 135);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Go!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(44, 153);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(31, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Port";
+            // 
+            // labelUnits
+            // 
+            this.labelUnits.AutoSize = true;
+            this.labelUnits.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUnits.Location = new System.Drawing.Point(362, 33);
+            this.labelUnits.Name = "labelUnits";
+            this.labelUnits.Size = new System.Drawing.Size(52, 29);
+            this.labelUnits.TabIndex = 3;
+            this.labelUnits.Text = "psi";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(280, 85);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(140, 62);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // labelPressure
+            // 
+            this.labelPressure.AutoSize = true;
+            this.labelPressure.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPressure.Location = new System.Drawing.Point(283, 33);
+            this.labelPressure.Name = "labelPressure";
+            this.labelPressure.Size = new System.Drawing.Size(73, 29);
+            this.labelPressure.TabIndex = 6;
+            this.labelPressure.Text = "00.0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(265, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Alarm Threshold (psi)";
+            // 
+            // numericUpDownAlarm
+            // 
+            this.numericUpDownAlarm.Location = new System.Drawing.Point(376, 153);
+            this.numericUpDownAlarm.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.numericUpDownAlarm.Name = "numericUpDownAlarm";
+            this.numericUpDownAlarm.Size = new System.Drawing.Size(51, 20);
+            this.numericUpDownAlarm.TabIndex = 7;
+            this.numericUpDownAlarm.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDownAlarm.ValueChanged += new System.EventHandler(this.numericUpDownAlarm_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 177);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDownAlarm);
+            this.Controls.Add(this.labelPressure);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelUnits);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Pressure Sensor, Russ Renzas, 2015";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAlarm)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelUnits;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label labelPressure;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDownAlarm;
+    }
+}
+
---
+++ PressureSensor/Form1.cs
@@ -0,0 +1,221 @@
+﻿//Pressure Sensor written by Russ Renzas, 2015
+//Use without attribution.
+//Purpose: Records input from Arduino board/program connected to a pressure sensor, alarms when psi > limit
+//Not tested beyond the immediate application it was written for. Arduino code included in github, but no wiring diagrams
+//Worked fine when everything was put together.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using System.IO.Ports;
+using System.Media;
+
+namespace PressureSensor
+{
+    public partial class Form1 : Form
+    {
+        bool currentlyMeasuring;
+        List<string> receivedData;
+        bool alarmFlag;
+        DateTime startTime;
+        DateTime last;
+        SoundPlayer player;
+
+        public Form1()
+        {
+            InitializeComponent();
+            currentlyMeasuring = false;
+            receivedData = new List<string>();
+            numericUpDown1.Value = PressureSensor.Properties.Settings.Default.portSet;
+            numericUpDownAlarm.Value = PressureSensor.Properties.Settings.Default.alarmValue;
+            player = new SoundPlayer();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (currentlyMeasuring) StopMeasurement();
+            else StartMeasurement();
+        }
+
+        private void StopMeasurement()
+        {
+            DialogResult dialogResult = MessageBox.Show("Save Data?","Wanna save?",MessageBoxButtons.YesNoCancel);
+            if (dialogResult == DialogResult.Cancel) return;
+            else if (dialogResult == DialogResult.Yes)
+            {
+                bool saved = SaveData();
+                if (!saved) return;
+            }
+
+            serialPort1.Close();
+            this.pictureBox1.Image = null;
+            currentlyMeasuring = false;
+            labelPressure.Text = "00.0";
+            alarmFlag = false;
+            this.BackColor = Form1.DefaultBackColor;
+            button1.Text = "Go!";
+            player.Stop();
+        }
+
+        private bool StartMeasurement()
+        {
+            receivedData.Clear();
+            serialPort1 = new System.IO.Ports.SerialPort("COM" + ((int)numericUpDown1.Value).ToString());
+            try
+            {
+                serialPort1.BaudRate = 9600;
+                serialPort1.Parity = System.IO.Ports.Parity.None;
+                serialPort1.StopBits = System.IO.Ports.StopBits.One;
+                serialPort1.DataBits = 8;
+                serialPort1.Handshake = System.IO.Ports.Handshake.None;
+                serialPort1.ReadTimeout = 5000;
+                serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(DataReceivedHandler);
+                serialPort1.Open();
+
+                button1.Text = "Stop";
+                numericUpDown1.Enabled = false;
+                this.pictureBox1.Image = Properties.Resources.animatedPies;
+                receivedData.Add("Time (min),psi");
+                alarmFlag = false;
+                this.BackColor = Form1.DefaultBackColor;
+                currentlyMeasuring = true;
+                startTime = new DateTime();
+                startTime = DateTime.Now;
+                last = new DateTime();
+                last = DateTime.Now;
+                return true;
+            }
+            catch
+            {
+                MessageBox.Show("Failed. Check connections and port.");
+                serialPort1.Close();
+                receivedData.Clear();
+                this.pictureBox1.Image = null;
+                currentlyMeasuring = false;
+                button1.Text = "Go!";
+                player.Stop();
+            }
+            return false;
+            
+        }
+           
+        private void ProcessRawData(string str)
+        {
+            //Add time before the string
+            double psi = double.Parse(str);
+            psi = psi / 1024 * 32;
+            str = Math.Round(psi, 1).ToString();
+            labelPressure.Text = str;
+            TimeSpan between = DateTime.Now.Subtract(last);
+            TimeSpan elapsed = DateTime.Now.Subtract(startTime);
+            if (!alarmFlag && Convert.ToDouble(numericUpDownAlarm.Value) < psi)
+            {
+                alarmFlag = true;
+                this.BackColor = Color.Red;
+                Random rand = new Random();
+                try
+                {
+                    player = new SoundPlayer();
+                    int i = rand.Next(3);
+                    if (i < 2) player.Stream = Properties.Resources.aoogah;
+                    else player.Stream = Properties.Resources.damn_it;
+                    player.Load();
+                    player.PlayLooping();
+                }
+                catch { }
+            }
+            
+            if (between.TotalSeconds >= 60)
+            {
+                string timeStat = Math.Round(elapsed.TotalMinutes,2,MidpointRounding.AwayFromZero).ToString();
+                receivedData.Add(timeStat + "," + str);
+                last = DateTime.Now;
+            }
+        }
+
+        private void DataReceivedHandler(
+                object sender,
+                SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string s = sp.ReadLine();
+            this.Invoke((MethodInvoker)delegate
+            {
+                ProcessRawData(s);
+            });
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            PressureSensor.Properties.Settings.Default.portSet = numericUpDown1.Value;
+            PressureSensor.Properties.Settings.Default.Save();
+        }
+
+        private bool SaveData()
+        {
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.OverwritePrompt = true;
+            saveFileDialog.FileName = "Pressure Data";
+            saveFileDialog.DefaultExt = "csv";
+            saveFileDialog.Filter = "Comma-delimited text files (*.csv)|*.csv";
+            saveFileDialog.InitialDirectory = PressureSensor.Properties.Settings.Default.saveDirectory;
+
+            try
+            {
+                    if (saveFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    {
+                        //buttonNewSample_Click(sender, e); //necessary to avoid error where it doesn't write current line of data
+                        System.IO.StreamWriter file = new System.IO.StreamWriter(saveFileDialog.OpenFile());
+                        WriteDataToFile(file);
+                        PressureSensor.Properties.Settings.Default.saveDirectory = System.IO.Path.GetDirectoryName(saveFileDialog.FileName);
+                        PressureSensor.Properties.Settings.Default.Save();
+                    }
+                    else
+                    {
+                        MessageBox.Show("File not saved.");
+                        return false;
+                    }
+            }
+            catch
+            {
+                MessageBox.Show("File open in another program. We'll keep doing what we were doing while you fix the situation and try again.");
+                return false;
+            }
+            return true;
+        }
+
+        private void WriteDataToFile(System.IO.StreamWriter file)
+        {
+            try
+            {
+                foreach (string s in receivedData)
+                {
+                    file.WriteLine(s);
+                }
+                file.Close();
+            }
+            catch { MessageBox.Show("Error. Check file name and data integrity."); return; }
+        }
+
+        private void numericUpDownAlarm_ValueChanged(object sender, EventArgs e)
+        {
+            PressureSensor.Properties.Settings.Default.alarmValue = numericUpDownAlarm.Value;
+            PressureSensor.Properties.Settings.Default.Save();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(currentlyMeasuring) StopMeasurement();
+        }
+
+    }
+}
